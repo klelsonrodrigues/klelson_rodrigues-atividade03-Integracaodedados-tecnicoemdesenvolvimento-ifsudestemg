01b8b25f700d29cf229291a86d426182306bcf02
klelsonrodrigues <117690197+klelsonrodrigues@users.noreply.github.com>
2024-07-30 20:49:21 -0300

    Add files via upload

---
+++ primeiroProjeto/index.js
@@ -0,0 +1,15 @@
+
+const express=require('express'); // determinando a utiliza 
+const app=express();
+const path=require ('path'); //enderço de cada rota
+const router=express.Router(); // trabalha com as rotas 
+router.get('/', function (req, res) {
+})
+res.sendFile(path.join(__dirname+'/index.html'));
+router.get('/about', function (req, res) {
+})
+res.sendFile(path.join(__dirname+'/about.html'));
+app.use('/', router);
+app.use('/about', router);
+app.listen(process.env.port||3000);
+console.log("Servidor rodando");
